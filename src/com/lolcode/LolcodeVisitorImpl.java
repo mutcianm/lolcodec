48b3db55111ca4c5460f8b8c8e1124c1df67d873
NinerLP <ninerlp@gmail.com>
2013-06-30 11:07:07 +0400

    added choice when we have addexpr (which can be + and -) and similar

--- src/com/lolcode/LolcodeVisitorImpl.java
+++ src/com/lolcode/LolcodeVisitorImpl.java
@@ -64,9 +64,18 @@ public class LolcodeVisitorImpl<T> extends lolcodeBaseVisitor<T> {
         return visitChildren(ctx);
     }
 
-    @Override public T visitVisstat(lolcodeParser.VisstatContext ctx) { return visitChildren(ctx); }
+    @Override public T visitVisstat(lolcodeParser.VisstatContext ctx) {
+        //do we printf here? https://github.com/thomaslee/llvm-demo/blob/master/main.cc http://tomlee.co/2012/11/calling-printf-via-the-llvm-3-1-cxx-api/
+        //because printing stuff in llvm looks kinda sad :(
+        //although i think it's makeable because we always print "%type\n" type = d|f|b or we print string\n
+        //so we get file type and we can write funcs like generatePrintStr generatePrintInt or something, i dunno i drunk jk
+        return visitChildren(ctx);
+    }
 
-    @Override public T visitNotexpr(lolcodeParser.NotexprContext ctx) { return visitChildren(ctx); }
+    @Override public T visitNotexpr(lolcodeParser.NotexprContext ctx) {
+        //to make things easier we can say we return 1 only if var is of "FAIL" dunno
+        return visitChildren(ctx);
+    }
 
     @Override public T visitCasestat(lolcodeParser.CasestatContext ctx) { return visitChildren(ctx); }
 
@@ -82,7 +91,21 @@ public class LolcodeVisitorImpl<T> extends lolcodeBaseVisitor<T> {
 
     @Override public T visitFunexpr(lolcodeParser.FunexprContext ctx) { return visitChildren(ctx); }
 
-    @Override public T visitMinmaxexpr(lolcodeParser.MinmaxexprContext ctx) { return visitChildren(ctx); }
+    @Override public T visitMinmaxexpr(lolcodeParser.MinmaxexprContext ctx) {
+        // min or max, actually it is not supposed to matter when we pass left and right expression
+        // until we get them both, and then we decide what %if llvm shit we put in
+        // because minmax is done with IF clauses in VM's, right?
+
+        //get lexpr, repxr
+        if (ctx.getChild(0).getText().equals("BIGGR OF")) {
+            return visitChildren(ctx);
+        } else if (ctx.getChild(0).getText().equals("SMALLR OF")) {
+            return visitChildren(ctx);
+        } else {
+            //not supposed to get here as usual
+            return visitChildren(ctx);
+        }
+    }
 
     @Override public T visitModexpr(lolcodeParser.ModexprContext ctx) { return visitChildren(ctx); }
 
@@ -93,8 +116,19 @@ public class LolcodeVisitorImpl<T> extends lolcodeBaseVisitor<T> {
         LolVariable rhs = (LolVariable)visit(ctx.expr(2));
         LolVariable tmp = new LolAbstractVariable(String.valueOf(currentFunction.symbols.size()+1), "FUCK_THIS_SHIT");
         currentFunction.addVariable(tmp);
-        currentFunction.addAddInst(tmp, lhs, rhs);
-        return (T)tmp;
+        if (ctx.getChild(0).getText().equals("SUM OF")) {
+            //add
+            currentFunction.addAddInst(tmp, lhs, rhs);
+            return (T)tmp;
+        } else if (ctx.getChild(0).getText().equals("DIFF OF")){
+            //diff
+            //i just copied Addexpr sum implementanion, none of that shit works btw
+            currentFunction.addAddInst(tmp, lhs, rhs);
+            return (T)tmp;
+        } else {
+            //we are not supposed to get here at all
+            return visitChildren(ctx);
+        }
     }
 
     @Override public T visitStat(lolcodeParser.StatContext ctx) { return visitChildren(ctx); }
@@ -115,9 +149,42 @@ public class LolcodeVisitorImpl<T> extends lolcodeBaseVisitor<T> {
 
     @Override public T visitEitherexpr(lolcodeParser.EitherexprContext ctx) { return visitChildren(ctx); }
 
-    @Override public T visitEquexpr(lolcodeParser.EquexprContext ctx) { return visitChildren(ctx); }
+    @Override public T visitEquexpr(lolcodeParser.EquexprContext ctx) {
+        //kinda same with minmax, where actual SAME or DIFFRNT matters only at the very end
+        //also is type checking done by llvm? can we do %i32 3 equals %float 3.0 and will it blend?
 
-    @Override public T visitMultexpr(lolcodeParser.MultexprContext ctx) { return visitChildren(ctx); }
+        if (ctx.getChild(0).getText().equals("BOTH SAEM")) {
+            return visitChildren(ctx);
+        } else if (ctx.getChild(0).getText().equals("DIFFRINT")) {
+            return visitChildren(ctx);
+        } else {
+            //as per usual never supposed to get here
+            return visitChildren(ctx);
+        }
+    }
+
+    @Override public T visitMultexpr(lolcodeParser.MultexprContext ctx) {
+        System.out.println(ctx.getChild(3).getText());
+        LolVariable lhs = (LolVariable)visit(ctx.getChild(1));
+        LolVariable rhs = (LolVariable)visit(ctx.getChild(3));
+        LolVariable tmp = new LolAbstractVariable(String.valueOf(currentFunction.symbols.size()+1), "FUCK_THIS_SHIT");
+        currentFunction.addVariable(tmp);
+        if (ctx.getChild(0).getText().equals("PRODUKT OF")) {
+            // multiplication
+            //is it supposed to be that way?
+
+            currentFunction.addMulInst(tmp, lhs, rhs);
+            return (T)tmp;
+        } else if (ctx.getChild(0).getText().equals("QUOSHUNT OF")) {
+            //division
+            //same
+            currentFunction.addDivInst(tmp, lhs, rhs);
+            return (T)tmp;
+        } else {
+            //some wrong shit, is it even possible to get here at all?
+            return visitChildren(ctx);
+        }
+        }
 
     private LolFunction currentFunction;
     private ArrayList<LolFunction> functions;
